dbbbfb10b52139fe433a151cda426f41fedce66a
mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2021-01-15 19:19:22 +0100

    add getter in result

--- lib/result.dart
+++ lib/result.dart
@@ -2,13 +2,28 @@ import 'package:flutter/cupertino.dart';
 
 class Result extends StatelessWidget {
   final int resultScore;
+
   Result(this.resultScore);
+
+  String get resultPhrase {
+    var resultText = 'You did it!';
+    if (resultScore <= 5) {
+      resultText = "Noob";
+    } else if (resultScore <= 9) {
+      resultText = 'You\'re good';
+    } else {
+      resultText = 'Excellent';
+    }
+    return resultText;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text("Your score is: $resultScore"),
+      child: Text(
+        resultPhrase,
+        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+      ),
     );
   }
 }
-
